b88d18995e34067f661e22572f1a5385441bc496
Nehith Sai Vemulapalli <87531686+nehith23@users.noreply.github.com>
2023-12-05 13:00:11 +0530

    Add files via upload

---
+++ src/classify_spam.m
@@ -0,0 +1,19 @@
+function [result] = classify_spam(document_vector,likelihoods_class_0,likelihoods_class_1,log_prior_class_0,log_prior_class_1)
+
+feature_vector=[];
+for element=1:length(document_vector)
+    if document_vector(element)> 0
+        feature_vector=[feature_vector 1];
+    else
+        feature_vector=[feature_vector 0];
+    end
+end
+
+[log_posterior_class_0,log_posterior_class_1] = calculate_class_posteriors(feature_vector,likelihoods_class_0,likelihoods_class_1,log_prior_class_0,log_prior_class_1);
+if log_posterior_class_0 > log_posterior_class_1
+    result=0;
+else
+    result=1;
+end
+end
+
